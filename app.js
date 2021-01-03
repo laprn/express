e6a1661a17d75ab3e8fd7c735e6858b15ebb8083
Ubuntu User <ubuntu@ik1-404-34174.vs.sakura.ne.jp>
2021-01-03 09:48:40 +0900

    remove header from 404

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   //next(createError(404));
   res.status(404);
+  res.removeHeader('X-Powered-By');
   res.render('404', {title: '404.'});
 });
 
