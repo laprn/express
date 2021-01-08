ddbe1a8d66c3e5b9f3ad2623cce496961089e35c
laprn <y.mrt@tuta.io>
2021-01-08 16:37:43 +0900

    recommit

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(function(req, res, next) {
   //next(createError(404));
   res.status(404);
   res.removeHeader('X-Powered-By');
+  res.removeHeader('server');
   res.render('404', {title: '404.'});
 });
 
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'ubuntu',
+	password: '3gFRpfmysql',
+	database: 'express_db'
+})
+
+connection.connect(()=>{
+  console.log('Connected');
+});
+
 router.get('/', function(req, res, next) {
   res.removeHeader('X-Powered-By');
+  res.removeHeader('server');
+  const sql = 'select * from ps_account';
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  res.render('index', { title: 'Index Page.', ip: ip });
+
+  connection.query(sql, function(err, result, fields){
+    if (err) throw err;
+    res.render('index', { title: 'Index Page.', ip: ip, sql_response: result });
+  });
 });
 
 module.exports = router;
