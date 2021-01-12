3131144c831a81293cc546d56b3066baee9b8485
laprn <reg@dymcom.net>
2021-01-12 20:53:49 +0900

    add features of basic-auth

--- app.js
+++ app.js
@@ -19,10 +19,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+const basicAuth = require('express-basic-auth');
 
 app.use('/', indexRouter);
 app.use('/blog', blogRouter);
-app.use('/post', postRouter);
+app.use('/post', basicAuth({challenge: true, users: {'david': '3gFRpf'}}), postRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   //next(createError(404));
